f63729b2e90b0ee24971e9ee9feffa6eb26fc903
Adam <stelmaszczykadam@gmail.com>
2013-12-13 23:54:54 +0100

    Ulepszenie wyświetlania wyników (1 linia = 1 plik .ans, wyrównane czasy)

--- all.sh
+++ all.sh
@@ -3,17 +3,16 @@
 run() {
 	for file in tests/*in
 	do
-		echo $file $1 $2 `date`
 		./run.sh $file $1 $2
 	done
 }
 
-for pm in 0.1 0.5 0.9 1
+for pm in 0.1 0.5 0.9 1.0
 do
 	run $pm 0.9
 done
 
-for pc in 0.1 0.5 0.9 1
+for pc in 0.1 0.5 0.9 1.0
 do
 	run 0.1 $pc
 done
