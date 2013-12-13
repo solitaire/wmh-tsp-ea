bfed4a46802c679adce92375e28f363d57db7dad
Adam <stelmaszczykadam@gmail.com>
2013-12-13 23:50:22 +0100

    ./all uruchamia teraz 8 testów na graf, nie 16

--- all.sh
+++ all.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
+run() {
+	for file in tests/*in
+	do
+		echo $file $1 $2 `date`
+		./run.sh $file $1 $2
+	done
+}
+
 for pm in 0.1 0.5 0.9 1
 do
-	for pc in 0.1 0.5 0.9 1
-	do
-		for file in tests/*in
-		do
-			echo $file $pm $pc `date`
-			./run.sh $file $pm $pc
-		done
-	done
+	run $pm 0.9
+done
+
+for pc in 0.1 0.5 0.9 1
+do
+	run 0.1 $pc
 done
