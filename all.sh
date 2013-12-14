24fa2e879801f51ec869ba32cccdbc59e4d825e8
Adam <stelmaszczykadam@gmail.com>
2013-12-14 15:15:50 +0100

    Skrypt png.r generujący wykresy, drobna poprawka w all.sh, odpowiedzi i wykres dla 17.in

--- all.sh
+++ all.sh
@@ -7,7 +7,7 @@ run() {
 	done
 }
 
-for pm in 0.1 0.5 0.9 1.0
+for pm in 0.5 0.9 1.0
 do
 	run $pm 0.9
 done
---
+++ png.r
@@ -0,0 +1,55 @@
+#!/usr/bin/Rscript
+
+n = commandArgs(TRUE)[1]
+
+if (is.na(n))
+{
+	stop("Usage: ./png.r 17")
+}
+
+filename = paste("tests/", n, ".png", sep = "")
+png(filename)
+colors = c("black", "darkblue", "cyan", "darkred", "red", "magenta", "green")
+names = rep(NA, 7)
+data = rep(NA, 7)
+i = 1
+
+for (pm in c(0.5, 0.9, "1.0"))
+{
+	pair = paste(pm, "_", 0.9, sep = "")
+	names[i] = pair
+	name = paste("tests/", n, "_", pair, ".ans", sep = "")
+	data[i] = read.table(name)
+	i = i + 1
+}
+
+for (pc in c(0.1, 0.5, 0.9, "1.0"))
+{
+	pair = paste(0.1, "_", pc, sep = "")
+	names[i] = pair
+	name = paste("tests/", n, "_", pair, ".ans", sep = "")
+	data[i] = read.table(name)
+	i = i + 1
+}
+
+optimum = scan(paste("tests/", n, ".out", sep = ""))
+for (i in 1:7)
+{
+	data[[i]] = data[[i]] - optimum
+}
+
+xmax = 0
+for (i in 1:7)
+{
+	xmax = max(xmax, max(data[[i]]))
+}
+
+plot(ecdf(data[[1]]), verticals = TRUE, do.points = FALSE, col = colors[1], main = n, xlab="najlepszy - optimum", ylab="Prawdopodobieństwo", xlim=c(0, xmax), ylim=c(0,1))
+for (i in 2:7)
+{
+	lines(ecdf(data[[i]]), verticals = TRUE, do.points = FALSE, col = colors[i])
+}
+
+legend("topleft", names, col = colors, lty = 1);
+
+print(filename)
