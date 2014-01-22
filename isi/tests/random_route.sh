8a2804e7a665c375b546d63c0fc500742a363baf
solitaire <solitaire@g.pl>
2014-01-22 22:06:15 +0100

    generator losowych miast + przykładowy plik wejściowy

---
+++ isi/tests/random_route.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ $# -lt 3 ]
+then
+	echo "Usage: $0 CITIES_FILE.in NUMBER_OF_CITIES NUMBER_OF_ROUTES"
+	return
+fi
+
+input=$1
+cities=$2
+routes=$3
+
+for r in $(seq 1 $routes)
+do
+	#shuf -n $cities $input > tests/$cities/$r.loc
+	echo tests/$cities/$r.loc
+	#mvn -q exec:java -Dexec.mainClass=graph.builder.GraphBuilder < tests/$cities/$r.loc > tests/$cities/$r.in
+done
